9fd535053b4a9b978dd68a5aad76d5c9da3640a1
Lonmo0208 <2675932757@qq.com>
2025-03-13 04:35:21 +0800

    !!!Fix!!!

--- common/src/main/java/me/jellysquid/mods/sodium/client/render/immediate/model/ModelCuboid.java
+++ common/src/main/java/me/jellysquid/mods/sodium/client/render/immediate/model/ModelCuboid.java
@@ -14,47 +14,63 @@ public class ModelCuboid {
     public final boolean mirror;
 
     private static final float INV16 = 1.0f / 16.0f;
+    private static final int[] DIRECTION_MASKS = new int[6];
 
-    public ModelCuboid(int u, int v, float x, float y, float z, float width, float height, float depth, float inflateX, float inflateY, float inflateZ, boolean mirror, float texWidth, float texHeight, Set<Direction> visibleFaces)
-    {float minX = (x - inflateX) * INV16;float maxX = (x + width + inflateX) * INV16;
+    static {
+        for (Direction dir : Direction.values()) {
+            DIRECTION_MASKS[dir.ordinal()] = 1 << dir.ordinal();
+        }
+    }
 
+    public ModelCuboid(int u, int v, float x, float y, float z, float width, float height, float depth, float inflateX, float inflateY, float inflateZ, boolean mirror, float texWidth, float texHeight, Set<Direction> visibleFaces) {
+        // Calculate mirrored X coordinates
+        float tempMinX = (x - inflateX) * INV16;
+        float tempMaxX = (x + width + inflateX) * INV16;
         if (mirror) {
-            float temp = maxX;
-            maxX = minX;
-            minX = temp;
+            this.x1 = tempMaxX;
+            this.x2 = tempMinX;
+        } else {
+            this.x1 = tempMinX;
+            this.x2 = tempMaxX;
         }
 
-        this.x1 = minX;
+        // Y and Z coordinates
         this.y1 = (y - inflateY) * INV16;
         this.z1 = (z - inflateZ) * INV16;
-        this.x2 = maxX;
         this.y2 = (y + height + inflateY) * INV16;
         this.z2 = (z + depth + inflateZ) * INV16;
 
+        // Texture scaling factors
         float scaleU = 1.0f / texWidth;
         float scaleV = 1.0f / texHeight;
 
+        // Precompute intermediate U values
+        float uDepth = u + depth;
+        float uDepthWidth = uDepth + width;
         this.u0 = scaleU * u;
-        this.u1 = scaleU * (u + depth);
-        this.u2 = scaleU * (u + depth + width);
-        this.u3 = scaleU * (u + depth + width + width);
-        this.u4 = scaleU * (u + depth + width + depth);
-        this.u5 = scaleU * (u + depth + width + depth + width);
+        this.u1 = scaleU * uDepth;
+        this.u2 = scaleU * uDepthWidth;
+        this.u3 = scaleU * (uDepthWidth + width);
+        this.u4 = scaleU * (uDepthWidth + depth);
+        this.u5 = scaleU * (uDepthWidth + depth + width);
 
+        // Precompute intermediate V values
+        float vDepth = v + depth;
         this.v0 = scaleV * v;
-        this.v1 = scaleV * (v + depth);
-        this.v2 = scaleV * (v + depth + height);
+        this.v1 = scaleV * vDepth;
+        this.v2 = scaleV * (vDepth + height);
 
         this.mirror = mirror;
 
+        // Generate face visibility mask using precomputed direction masks
         int mask = 0;
         for (Direction face : visibleFaces) {
-            mask |= 1 << face.ordinal();
+            mask |= DIRECTION_MASKS[face.ordinal()];
         }
         this.faces = mask;
     }
 
     public boolean shouldDrawFace(int faceIndex) {
-        return (this.faces & (1 << faceIndex)) != 0;
+        return (this.faces & DIRECTION_MASKS[faceIndex]) != 0;
     }
 }
