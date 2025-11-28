4a5718089c99add2b2ee2f485c649ca4fcea8486
Lonmo <2675932757@qq.com>
2025-11-29 01:47:54 +0800

    Bump version number to 0.8.0-beta.1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,14 +4,14 @@ object BuildConfig {
     val MINECRAFT_VERSION: String = "1.21.11-pre3"
     val NEOFORGE_VERSION: String = "21.11.0-alpha.1.21.11-pre3.20251126.164358"
     val FABRIC_LOADER_VERSION: String = "0.18.1"
-    val FABRIC_API_VERSION: String = "0.139.1+1.21.11"
+    val FABRIC_API_VERSION: String = "0.139.3+1.21.11"
     val SUPPORT_FRAPI : Boolean = false
 
     // This value can be set to null to disable Parchment.
     val PARCHMENT_VERSION: String? = null
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.7.3"
+    var MOD_VERSION: String = "0.8.0-beta.1"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
