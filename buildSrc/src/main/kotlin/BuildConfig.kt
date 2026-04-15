533d86bf087e267d7185de6f6a96fab222c461c9
Lonmo <95102577+Lonmo0208@users.noreply.github.com>
2026-04-16 05:30:49 +0800

    Update BuildConfig.kt

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,10 +1,10 @@
  import org.gradle.api.Project
 
 object BuildConfig {
-    val MINECRAFT_VERSION: String = "26.1.1"
+    val MINECRAFT_VERSION: String = "26w14a"
     val NEOFORGE_VERSION: String = "26.1.1.0-beta"
-    val FABRIC_LOADER_VERSION: String = "0.18.5"
-    val FABRIC_API_VERSION: String = "0.145.3+26.1.1"
+    val FABRIC_LOADER_VERSION: String = "0.19.1"
+    val FABRIC_API_VERSION: String = "0.145.2+26w14a"
     val SUPPORT_FRAPI : Boolean = true
 
     // This value can be set to null to disable Parchment.
